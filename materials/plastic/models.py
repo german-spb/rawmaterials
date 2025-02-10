9331e05b45ad4a192a004000f858113207bb2b30
german-spb <german@pushkin-spb.com>
2025-02-10 18:03:33 +0300

    change files models and index

--- materials/plastic/models.py
+++ materials/plastic/models.py
@@ -6,4 +6,17 @@ class Plastics(models.Model):
     code_contractor = models.CharField(max_length=20)
     name_contractor = models.CharField(max_length=50)
     price = models.IntegerField()
-    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.code_sbk
+
+class Stocks(models.Model):
+    name = models.ForeignKey(Plastics, on_delete=models.CASCADE)
+    quantity_3050 = models.IntegerField()
+    quantity_2440 = models.IntegerField()
+    quantity_4200 = models.IntegerField()
+    quantity_rol = models.IntegerField()
+
+
+
+
